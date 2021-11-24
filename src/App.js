5169bdb1505294a3ea5dfdb3ad5ec45dbffd8860
rutuja <rutujaingale0230@example.com>
2021-11-24 22:00:30 +0530

    redirecting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {BrowserRouter} from "react-router-dom";
 import './App.css';
 import { Header } from './components/Header';
@@ -5,19 +6,30 @@ import About from "./pages/About";
 
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Post from "./pages/Post";
 import Profile from "./pages/Profile";
 
 function App() {
+   const [login, setLogin ] =useState(false);
   return(
   <BrowserRouter basename = "/tutorial" >
     <div className="App">
+
   
 <Header/>
     </div>
-    <Route path ='/Home' component = {Home }/>
-    <Route path ='/About'component = {About}/>
-    <Route path = '/Profile'component = {Profile }/>
+    <button onclick={() => setLogin(!login)}>{login ? "log out" :"login"}
+    </button>
+    <Switch>
+    <Route path ="/Home" component = {Home }/>
+    <Route path ="/About"component = {About}/>
+    <Route path = "/Profile">
+      {login ? <Profile/> : <Redirect to ="/" />}
+      </Route>
+
+    <Route path ="/Post/:id"component ={Post} />
     <Route component = {NotFound} />
+    </Switch>
  </BrowserRouter>
   );
     
---
+++ src/pages/Post.js
@@ -0,0 +1,17 @@
+import { useLocation, useParams } from "react-router";
+
+const Post = () => {
+     const { id } =useParams();
+     const query = new URLSearchParams(useLocation().search);
+    return (
+        <>
+        <h2>
+           Id is ={id}
+        </h2>
+        <h2>{query.get("first")}</h2>
+        <h2>{query.get("last")}</h2>
+   </> 
+    );
+};
+
+export default Post;

ea6397245720587555c5a8f0b2ead43dc5e1bbdd
rutuja <rutujaingale0230@example.com>
2021-11-24 17:10:48 +0530

    browser react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-
+import {BrowserRouter} from "react-router-dom";
 import './App.css';
 import { Header } from './components/Header';
 
-
 function App() {
   return(
-  
+  <BrowserRouter>
     <div className="App">
+  
 <Header/>
     </div>
- 
+    <Route path ='/' component = { }/>
+    <Route path ='/about'component = { }/>
+    <Route path = '/profile'component = { }/>
+ </BrowserRouter>
   );
     
 }
